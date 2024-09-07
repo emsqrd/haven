b53e0489d6b2b3b6c169d9008c06266aa97b7ee9
Mark <madlyinanger37@gmail.com>
2024-09-07 09:33:55 -0500

    Router setup

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeDetailComponent } from './home-detail/home-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'detail', pathMatch: 'full' },
+  { path: 'detail', component: HomeDetailComponent },
+];
